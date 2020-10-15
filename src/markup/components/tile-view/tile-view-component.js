c11dea4e975af128a8bccc2f0b2c5810dd1f7664
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-15 11:21:45 +0530

    rendering cart items in dom

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -1,97 +1,32 @@
 $(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    
+    url: "../../../data/items.json",
+    success: function (res) {
+        var itemCountId = 0; 
+        $.each(res.items, function(key, value) {
+          $(".items-tile-section").append(`
+            <div class="item-details-container" data-id = "itemId-` + itemCountId + `">
+                <div class="item-image-section">
+                    <div class="item-discount">` + value.discount + `% off</div>
+                    <img src="` + value.image + `">
+                </div>
+                <div class="item-details-section">
+                    <div class="item-name">` + value.name + `</div>
+                    <div class="item-details">
+                        <div class="item-price">
+                            <span class="item-display-price" data-display="` + value.price.display +`">$` + value.price.display + `</span>
+                            <span class="item-actual-price" data-actual="` + value.price.actual + `">$` + value.price.actual + `</span>
+                        </div>
+                        <button class="add-to-cart-btn"> Add to Cart </button>
+                    </div>
+                </div>
+            </div>
+        `)
+          console.log(value.name)
 
-    // JSON DATA
-    var itemsList = {
-        "items": [
-          {
-            "name": "Samsung Series 4",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 13999,
-              "display": 22500
-            },
-            "discount": 37
-          },
-          {
-            "name": "Samsung Super 6",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 35999,
-              "display": 66900
-            },
-            "discount": 46
-          },
-          {
-            "name": "Samsung The Frame",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 84999,
-              "display": 133900
-            },
-            "discount": 36
-          },
-          {
-            "name": "Thomson B9 Pro",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 9999,
-              "display": 16999
-            },
-            "discount": 41
-          },
-          {
-            "name": "LG Ultra HD",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 39990,
-              "display": 79990
-            },
-            "discount": 50
-          },
-          {
-            "name": "Vu Ready LED TV",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 7999,
-              "display": 17e3
-            },
-            "discount": 52
-          },
-          {
-            "name": "Koryo Android TV",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinonedesktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 55999,
-              "display": 199990
-            },
-            "discount": 71
-          },
-          {
-            "name": "Micromax LED Smart",
-            "image": "https://rukminim1.flixcart.com/image/670/600/allinone-desktop/d/n/q/apple-imac-21-5-inch-4k-retina-core-i5-3-1ghz-8gb-1tb-inteliris-original-imaeh5h83fuzbksz.jpeg?q=90",
-            "price": {
-              "actual": 9999,
-              "display": 27990
-            },
-            "discount": 64
-          }
-        ]
-      }
-    
-   
-    
-    
-      $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "../../../data/items.json",
-        success: function (data) {
-          console.log(data);
-        },
-      });
-    
-     // $.getJSON("/src/assets/items.json", function(res) {
-      //     console.log(res.items.name)
-      // })
-    
-    });
+        })
+    },
+  });
+});
