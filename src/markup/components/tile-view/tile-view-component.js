383df27f66229087b2272b10fc2ebba9014f242f
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-15 19:03:06 +0530

    other components

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -17,9 +17,8 @@ function fetchItemData() {
 
 // passing data into renderData() and rendering it into DOM
 function renderData(renderItemData) {
-  var itemCountId = 0;
-  var itemList = renderItemData.map((data,index) => {
-    return `<div class="item-details-container" data-id = "itemId-` + itemCountId + `">
+  let itemList = renderItemData.map((data,index) => {
+    return `<div class="item-details-container" data-id = "itemId-${index}">
                 <div class="item-image-section">
                     <div class="item-discount"> ${data.discount} % off</div>
                     <img src="${data.image}">
@@ -31,12 +30,11 @@ function renderData(renderItemData) {
                             <span class="item-display-price" data-display="${data.price.display}">$${data.price.display}</span>
                             <span class="item-actual-price" data-actual="${data.price.actual}">$${data.price.actual}</span>
                         </div>
-                        <button class="add-to-cart-btn"> Add to Cart </button>
+                        <button class="add-to-cart-btn" id="btn-${index}"> Add to Cart </button>
                     </div>
                 </div>
             </div>
         `}).join(' ');
-      itemCountId++;
     
   // Passing/appending list data for rendering
   document.querySelector(".items-tile-section").innerHTML = itemList;
@@ -46,3 +44,6 @@ function renderData(renderItemData) {
 window.addEventListener('load', (event) => {
   fetchItemData();
 });
+
+// window.addEventListener('click', )
+document.getElementById("")
