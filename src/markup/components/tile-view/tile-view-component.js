0e5d7e73e4a4ba487fc60f1dbed86be3e1e775ea
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-15 12:06:21 +0530

    initial component

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -1,32 +1,16 @@
-$(document).ready(function () {
+const { event } = require("grunt");
+
+window.load(function() {
+
   $.ajax({
     type: "GET",
-    
     url: "../../../data/items.json",
-    success: function (res) {
-        var itemCountId = 0; 
-        $.each(res.items, function(key, value) {
-          $(".items-tile-section").append(`
-            <div class="item-details-container" data-id = "itemId-` + itemCountId + `">
-                <div class="item-image-section">
-                    <div class="item-discount">` + value.discount + `% off</div>
-                    <img src="` + value.image + `">
-                </div>
-                <div class="item-details-section">
-                    <div class="item-name">` + value.name + `</div>
-                    <div class="item-details">
-                        <div class="item-price">
-                            <span class="item-display-price" data-display="` + value.price.display +`">$` + value.price.display + `</span>
-                            <span class="item-actual-price" data-actual="` + value.price.actual + `">$` + value.price.actual + `</span>
-                        </div>
-                        <button class="add-to-cart-btn"> Add to Cart </button>
-                    </div>
-                </div>
-            </div>
-        `)
-          console.log(value.name)
-
-        })
-    },
+    success: function (res) {   
+      console.log(res)
+    }
   });
-});
+
+})
+
+  
+
