7e573f1a2073ff7d17c8a9917cc23a82703d71d7
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-15 13:26:02 +0530

    fetching data

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -1,16 +1,36 @@
-const { event } = require("grunt");
+// const { event } = require("grunt");
 
-window.load(function() {
+// window.load(function() {
 
-  $.ajax({
-    type: "GET",
-    url: "../../../data/items.json",
-    success: function (res) {   
-      console.log(res)
-    }
-  });
+//   $.ajax({
+//     type: "GET",
+//     url: "../../../data/items.json",
+//     success: function (res) {   
+//       console.log(res)
+//     }
+//   });
 
-})
+// })
 
   
 
+fetch('../../../data/items.json')
+  .then(res => res.json())
+  .then(data =>{
+    console.log(data)
+    if(data.length > 0) {
+      var test = '';
+
+      //-- start for loop
+
+      data.forEach((itemData) => {
+        test +="<div>"
+        test +="<div>"+ itemData.items.name +"</div></div>";
+      });
+
+      //-- end for loop
+console.log("data test",test)
+      document.getElementById("testData").innerHTML = test;
+    }
+
+  });
