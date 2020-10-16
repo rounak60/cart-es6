69f5f5cc6e2d0fef496e3ba781316ed7ec7cc3f8
rounakMoonraft <49684813+rounakMoonraft@users.noreply.github.com>
2020-10-16 12:26:00 +0530

    total and calc list components

--- src/markup/components/tile-view/tile-view-component.js
+++ src/markup/components/tile-view/tile-view-component.js
@@ -17,37 +17,53 @@ function fetchItemData() {
 }
 
 function addListner(data) {
-  let cartDetails = data.map((cartBtn,index) => {
+   data.map((cartBtn,index) => {
+    let htmlTable = "";
+
+    function cartBtnProp() {
+      document.querySelector(`#btn-${index}`).setAttribute('disabled','true');
+      document.querySelector(`#btn-${index}`).className += " " + "add-to-cart-btn--disabled";
+      document.querySelector(`#btn-${index}`).innerHTML = "Added to Cart";
+      document.querySelector(".header-toast-msg").style.display = "block";
+      document.querySelector(".toast-msg-text").innerHTML = cartBtn.name + " is added to cart";
+    }
+
     document.querySelector(`#btn-${index}`).addEventListener("click", function (e) {
 
-      const grossTotal = parseInt(document.getElementsByClassName("gross-total")[0].innerText);
-      const discTotal = parseInt(document.getElementsByClassName("disc-total")[0].innerText);
       const itemTotal = parseInt(document.getElementsByClassName("item-total")[0].innerText);
+      const discTotal = parseInt(document.getElementsByClassName("disc-total")[0].innerText);
+      const grossTotal = parseInt(document.getElementsByClassName("gross-total")[0].innerText);
       const newDiscount = cartBtn.price.display - cartBtn.price.actual;
-
+      
+      document.getElementsByClassName("item-total")[0].innerText =  (itemTotal + cartBtn.price.display);
       document.getElementsByClassName("disc-total")[0].innerText =  (discTotal + newDiscount);
       document.getElementsByClassName("gross-total")[0].innerText =  (grossTotal + cartBtn.price.actual);
-      document.getElementsByClassName("item-total")[0].innerText =  (itemTotal + cartBtn.price.display);
 
-      return `<tr> 
-                <th> 
-                  <div>
-                    <div>${cartBtn.image}</div>
-                    <div>${cartBtn.name}</div>
-                  </div> 
-                </th> 
-                <th>
-                  <input type="button" value="-" class="sub-quant" >
-                  <input type="text" size="10" value="1" class="totalCount" disabled>
-                  <input type="button" value="+" class="add-quant" >
-                </th> 
-                <th>$${cartBtn.price.display}</th> 
-              </tr>
-              `
+
+      cartBtnProp(); // Function for Cart Button properties change on click.
+
+      htmlTable =  `<tr> 
+                      <td> 
+                        <div class="item-list-details">
+                          <div class="item--details">
+                            <div>
+                              <img class="item-list-image" src="${cartBtn.image}">
+                            </div>
+                            <div class="item-list-name">${cartBtn.name}</div>
+                          </div>
+                          <div class="item-list-remove">x</div>
+                        </div> 
+                      </td> 
+                      <td class="list-item-qty">
+                        <input type="button" value="-" class="sub-quant" >
+                        <input type="text" size="10" value="1" class="totalCount" disabled>
+                        <input type="button" value="+" class="add-quant" >
+                      </td> 
+                      <td class="list-item-price">$${cartBtn.price.display}</td> 
+                    </tr>`
+      document.querySelector(".added-items-list").innerHTML += htmlTable;
     });
-  }).join(' ');
-  document.querySelector(".added-items-list").innerHTML = cartDetails;
-
+  });
 }
 
 function addQty() {
